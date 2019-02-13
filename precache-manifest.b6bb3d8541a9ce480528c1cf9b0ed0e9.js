b4a59cfdafde0838a521329b010dcb738bdad0eb
whwz <wachowicz@vp.pl>
2019-02-13 22:28:24 +0100

    Updates

---
+++ precache-manifest.b6bb3d8541a9ce480528c1cf9b0ed0e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b24b864541298412a2ae",
+    "url": "/movie-search/static/js/runtime~main.b24b8645.js"
+  },
+  {
+    "revision": "d85fb9f5501aeeee8a41",
+    "url": "/movie-search/static/js/main.d85fb9f5.chunk.js"
+  },
+  {
+    "revision": "0f0f1ed395de9ee32ca5",
+    "url": "/movie-search/static/js/2.0f0f1ed3.chunk.js"
+  },
+  {
+    "revision": "d85fb9f5501aeeee8a41",
+    "url": "/movie-search/static/css/main.b8959b19.chunk.css"
+  },
+  {
+    "revision": "b78d3a52cf2759fdda114277c2cd4c8d",
+    "url": "/movie-search/index.html"
+  }
+];
