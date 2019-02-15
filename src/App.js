d54d4f1979b7c61ffadc5dc5d47b5b3b1edb2c58
whwz <wachowicz@vp.pl>
2019-02-15 13:39:17 +0100

    add loader

--- src/App.js
+++ src/App.js
@@ -15,18 +15,19 @@ class App extends Component {
   componentDidMount() {
     let page = 1;
 
-    while(page < 51){
+    while(page < 36){
       fetch("https://api.themoviedb.org/3/movie/top_rated?page=" + page + "&language=en-US&api_key=9d59af51a07ce660e8606073f1dfe0ec")
       .then(res => res.json())
       .then(json => {
         for(let i = 0; i < json["results"].length; i++)
-          db.push(json.results[i])
+          if(json.results[i].vote_count > 2000)
+            db.push(json.results[i])
         });
 
       page++;
     }
 
-    setTimeout(()=>{this.setState({movies: db})}, 1000);
+    setTimeout(()=>{this.setState({movies: db})}, 3000);
   }
 
   filterMovies = (e) => {
@@ -45,29 +46,30 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <input onInput={this.filterMovies}></input>
-        <MoviesList movies={this.state.filteredMovies} />
+        <input onInput={this.filterMovies} placeholder="Search..." />
+        <MoviesList filteredMovies={this.state.filteredMovies} movies={this.state.movies} />
       </div>
     );
   }
 }
 
-const MoviesList = ({movies}) => {
+const MoviesList = ({filteredMovies, movies}) => {
   const link = "https://image.tmdb.org/t/p/w185_and_h278_bestv2";
   return (
-    <div className="cards">
-
-      {movies.map(movie => 
-        movie.vote_count > 2000 ?
-          <div className="item" key={movie.title}>
-            <img src={link + movie.poster_path} alt="" />
-            <p>{movie.title}</p>
-            <p>{movie.vote_count}</p>
-          </div> 
-        : null
-      )}
-
-    </div>
+    movies.length ?
+      filteredMovies.length ?
+        <div className="cards">
+          {filteredMovies.map(movie => 
+            <div className="item" key={movie.title}>
+              <img src={link + movie.poster_path} alt="" />
+              <p>{movie.title}</p>
+            </div> 
+          )}
+        </div>
+        :
+        <p className="no_results">No results</p>
+      :
+      <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
   )
 }
 
